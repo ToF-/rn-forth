c065e8266b3fcaf3855c8ff812c83a6f240291f8
Christophe Thibaut <cthibauttof@gmail.com>
2022-08-19 22:32:55 +0200

    refactoring: use a loop

--- rn.fs
+++ rn.fs
@@ -1,11 +1,10 @@
 create romans 10 allot
 
+
 : decimal>roman ( u -- )
-  1 = if
-    1 romans c!
-    [char] I romans 1+ c! 
-  else
-    2 romans c!
-    [char] I romans 1+ c!
-    [char] I romans 2 + c! 
-  then ;
+  0 romans c!
+  0 do
+    romans dup c@ 1+ swap c!
+    [char] I romans 1+ i + c! 
+  loop ;
+
