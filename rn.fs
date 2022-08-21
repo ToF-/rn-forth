a7f62f6b58705fc8c29b05633e4e7511fef81ec6
Christophe Thibaut <cthibauttof@gmail.com>
2022-08-21 13:20:57 +0200

    Leo Brodie's version

--- rn.fs
+++ rn.fs
@@ -1,6 +1,46 @@
-create romans 
-char I c, char V c, char X c, char L c, char C c, char D c, char M c,
+create romans char I c, char V c,
+              char X c, char L c,
+              char C c, char D c,
+              char M c,
 
-: roman-digit ( u -- c )
-  romans + c@ ;
+variable column#
 
+: ones 0 column# ! ;
+: tens 2 column# ! ;
+: hundreds 4 column# ! ;
+: thousands 6 column# ! ;
+
+: column ( -- address of column )
+  romans column# @ + ;
+
+: .symbol ( offset -- )
+  column + c@ emit ;
+
+: oner
+  0 .symbol ;
+
+: fiver
+  1 .symbol ;
+
+: tener
+  2 .symbol ;
+
+: oners ( # of oners -- )
+  ?dup if 0 do oner loop then ;
+
+: almost ( quotient of 5 / -- )
+  oner if tener else fiver then ;
+
+: digit ( digit -- )
+  5 /mod over 4 = if 
+    almost drop 
+  else 
+    if fiver then 
+    oners 
+  then ;
+
+: roman ( number -- )
+  1000 /mod thousands digit
+   100 /mod  hundreds digit
+    10 /mod      tens digit
+                 ones digit ;
--- tests.fs
+++ tests.fs
@@ -3,8 +3,53 @@ require rn.fs
 
 page
 
-." roman digits table" cr
+." romans table" cr
 t{ 
-  0 roman-digit char I ?s
+  romans c@ char I ?s  
+}t
+." column# variable" cr
+t{
+  ones column# @ 0 ?s
+  tens column# @ 2 ?s
+  hundreds column# @ 4 ?s
+  thousands column# @ 6 ?s
+}t
+." column" cr
+t{
+  hundreds column c@ char C ?s
+}t
+." .symbol" cr
+t{
+  tens 1 .symbol \ should emit a L
+  ones 1 .symbol \ should emit a V
+  cr
+}t
+." oner, fiver, tener" cr
+t{
+  hundreds
+  oner \ should emit a C
+  fiver \ should emit a D
+  tener \ should emit a M
+  cr
+}t
+." oners" cr
+t{ 
+  thousands 3 oners \ should emit MMM
+  cr
+}t
+." almost" cr
+t{
+  tens 0 almost cr \ should emit XL
+  1 almost cr \ should emit XC
+}t
+." digit" cr
+t{
+    tens 7 digit \ should emit LXX
+    cr
+}t
+." roman" cr
+t{
+    3827 roman \ should emit MMMDCCCXXVII
+    cr
 }t
 bye
